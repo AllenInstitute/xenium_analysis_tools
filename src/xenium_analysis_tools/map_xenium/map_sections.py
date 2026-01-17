5f70488475af28016dbfbd7b1a7b7633d1e678f4
Hannah Schryver <97698939+hschryver@users.noreply.github.com>
2026-01-16 18:30:57 -0800

    Fix save path for v1 types df

--- src/xenium_analysis_tools/map_xenium/map_sections.py
+++ src/xenium_analysis_tools/map_xenium/map_sections.py
@@ -53,7 +53,7 @@ def get_v1_merfish_cells(abc_cache=None, df_path=None):
         # Get MERFISH CCF metadata
         print('V1 cell df not found, generating new one...')
         if abc_cache is None:
-            raise ValueError("abc_cache must be provided if output_path does not exist")
+            raise ValueError("abc_cache must be provided if path to df does not exist")
         merfish_ccf_metadata = abc_cache.get_metadata_dataframe(
                     directory='MERFISH-C57BL6J-638850-CCF', 
                     file_name='cell_metadata_with_parcellation_annotation'
@@ -61,7 +61,7 @@ def get_v1_merfish_cells(abc_cache=None, df_path=None):
         v1_merfish_cells = merfish_ccf_metadata.loc[merfish_ccf_metadata['parcellation_structure']=='VISp']
         # Save created df
         print(f"Saving df to: {df_path}")
-        v1_merfish_cells.to_csv(output_path)
+        v1_merfish_cells.to_csv(df_path)
     return v1_merfish_cells
 
 def get_nodes_to_drop(cells_df, abc_cache, h_level='subclass', min_cells=0):
